c7d7702b78f959885bc7f70f61f9ff4654aa3c86
ivanbrennan <ivan.brennan@gmail.com>
2017-05-31 18:36:04 -0400

    better messaging

--- install-ctags.sh
+++ install-ctags.sh
@@ -12,11 +12,11 @@ main() {
   echo '### Checking ctags executable...'
 
   if universal_ctags_installed; then
-    echo '### Your ctags is already up-to-date! (https://ctags.io)'
+    echo '--> Your ctags is already up-to-date! (https://ctags.io)'
   elif exuberant_ctags_installed; then
     verify_upgrade
   else
-    echo "### Installing ctags..."
+    echo "    Installing ctags..."
     install_universal_ctags
   fi
 }
@@ -31,15 +31,19 @@ exuberant_ctags_installed() {
 
 verify_upgrade() {
   cat <<EOF
-You currently have Exuberant Ctags (http://ctags.sourceforge.net) installed.
-This works fine, but is no longer actively-maintained.
-Universal Ctags (https://ctags.io) is recommended.
+
+    You currently have Exuberant Ctags (http://ctags.sourceforge.net) installed.
+    This works fine, but is no longer actively-maintained.
+    Universal Ctags (https://ctags.io) is recommended.
+
 EOF
-  if verify 'Do you want to replace Exuberant Ctags with Universal Ctags?'; then
+  if verify '  Do you want to replace Exuberant Ctags with Universal Ctags?'; then
+    echo '    Uninstalling Exuberant Ctags...'
     uninstall_exuberant_ctags
+    echo '    Installing Universal Ctags'
     install_universal_ctags
   else
-    echo '### Using Exuberant Ctags'
+    echo '    Using Exuberant Ctags'
   fi
 }
 
@@ -50,7 +54,7 @@ verify() {
     if [ "$ans" = "no" ]; then
       exit 1
     elif [ "$ans" != "yes" ]; then
-      echo "Invalid response..."
+      echo "  Invalid response..."
       verify "$1"
     fi
   )
