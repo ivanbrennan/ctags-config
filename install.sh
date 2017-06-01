8a7e378d138b86b78aa57438561af4ecd64fff1d
ivanbrennan <ivan.brennan@gmail.com>
2017-06-01 08:13:31 -0400

    clean up install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,11 @@ set -eu
 
 TEMP_DIR=$1
 
-col_green="\x1b[32;01m"
-col_reset="\x1b[39;49;00m"
-green_check="${col_green}✓${col_reset}"
-
 bash ${TEMP_DIR}/ctags-config/install-ctags.sh
 bash ${TEMP_DIR}/ctags-config/configure-ctags.sh
 bash ${TEMP_DIR}/ctags-config/configure-git-template-directory.sh
 bash ${TEMP_DIR}/ctags-config/install-ctags-git-hooks.sh ${TEMP_DIR}
 
-echo -e "Finished successfully $green_check"
+col_green="\x1b[32;01m"
+col_reset="\x1b[39;49;00m"
+echo -e "Finished successfully ${col_green}✓${col_reset}"
