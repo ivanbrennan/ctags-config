35383efb47d0e2391f778d7cf6e891bc782041b9
ivanbrennan <ivan.brennan@gmail.com>
2017-06-01 08:48:05 -0400

    Done

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ bash ${TEMP_DIR}/ctags-config/install-ctags-git-hooks.sh ${TEMP_DIR}
 
 col_green="\x1b[32;01m"
 col_reset="\x1b[39;49;00m"
-echo -e "Finished successfully ${col_green}✓${col_reset}"
+echo -e "Done ${col_green}✓${col_reset}"
